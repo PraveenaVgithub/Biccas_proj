7f79aec543532fc58a4c72c7f4a0ec3c31a45aa2
PraveenaVgithub <praveenaoffid@gmail.com>
2024-11-26 22:52:42 +0530

    construction update

--- src/pages/components/faq/Faq.jsx
+++ src/pages/components/faq/Faq.jsx
@@ -6,7 +6,7 @@ const Faq = () => {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
       <div className="text-center">
-        <h1 className="display-4 text-danger faqtxt">Under Construction 👷‍♀️⚠️</h1>
+        <h1 className="display-4 text-danger faqtxt">Under Construction.. 👷‍♀️⚠️</h1>
         <p className="lead text-muted">We're working hard to bring you the best experience. Please check back later!</p>
         <div className="mt-4">
           <button className="faqbut" onClick={()=>{(navigate('/'))}}>Go to Homepage</button>
